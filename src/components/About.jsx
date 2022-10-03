f04f3e9bcdcadb76658a0cd19d5a284fb0b46b09
Kingkp007 <patilkshitish@gmail.com>
2022-10-03 23:07:34 +0530

    changes in about page and heading of home page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,24 +9,11 @@ const About = () => {
                 <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
             </div>
             <p className="text-xl mt-20">
-            Lorem ipsum dolor sit amet, 
-            consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut 
-            labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud 
-            exercitation ullamco laboris nisi ut 
-            aliquip ex ea commodo consequat. 
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut 
+            Hi, I am a Mechanical Engineer by degree and a Web developer by profession. I completed my Bachelor's degree from Savitribai Phule Pune University in 2021 with First class with distinction(8.7 CGPA). I learned programming languages when I was in college and I found interest in it so I decided to pursue my career in computer science field.
             </p>
             <br />
             <p className="text-xl">
-            labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud 
-            exercitation ullamco laboris nisi ut 
-            aliquip ex ea commodo consequat. 
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut 
+            I like to read books, play video games. Also I am a part time trader in stock market 
             </p>
         </div>
     </div>
